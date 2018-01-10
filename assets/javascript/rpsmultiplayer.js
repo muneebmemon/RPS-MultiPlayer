c5dc934d8ac6342913e38b49d8675ce65a11b90f
muneebmemon <muneeb.memon@gmail.com>
2018-01-09 20:20:19 -0600

    Chat feature was included

--- assets/javascript/rpsmultiplayer.js
+++ assets/javascript/rpsmultiplayer.js
@@ -1,3 +1,4 @@
+// Firebase configuration
 var config = {
     apiKey: "AIzaSyCljw7yDl7I4_qpJ2I9_3GHXWB1SJZcrOk",
     authDomain: "rps-e7edf.firebaseapp.com",
@@ -7,17 +8,43 @@ var config = {
     messagingSenderId: "785765240677"
 };
 
+// This is document.ready event
 $(function() {
     firebase.initializeApp(config);
+
+    //getting reference to database
 	database = firebase.database();
 	playerRef = database.ref("/Player");
 	rootRef = database.ref();
-	startGameRef = database.ref("/startGame");
+	chatRef = rootRef.child("/chat")
 
+	chatArea = $("#chatArea");
+	chatArea.val("-----Chat Messages appear here-----");
+
+	//This event is occured when player starts game by clicking start button
 	$("#startButton").on("click", function(){
-		startGameRef.set({
-			startGame: "true"
-		});
+		 playerName = $("#playerName").val().trim();		
+		 sessionStorage.setItem("playerName",playerName);
 	});
 
+	// This event is occured when player send chat message
+	$("#sendMessage").on("click", function(e){
+		 var textMsg = $("#textMessage");
+		 e.preventDefault();
+		 chatRef.set({
+		 	chatmsg: sessionStorage.getItem("playerName") + " : " + textMsg.val().trim()
+		 });
+		 textMsg.val("");
+	});	
+
+	//This event is occured after players send chat messages
+	chatRef.on("value", function(snapshot) {
+		var newLine = "\r\n";
+		var chatMsg = snapshot.val().chatmsg;
+		var chatText = chatArea.val();
+		chatArea.val(chatText + newLine + chatMsg);
+		chatRef.set({
+		 	chatmsg: " "
+		 });
+	});
 });
